a1d317876700e66a385f71e06ef369c1959ab8fa
Laejun Kim <49670820+Laejun-Kim@users.noreply.github.com>
2023-12-06 11:47:49 +0900

    Feat:"추가 map컴포넌트 작성중"

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
+import Maps from 'components/Maps';
+
 const App = () => {
-    return <div>App</div>;
+    return (
+        <>
+            <div>App</div>
+            <div style={{ width: '700px', height: '500px' }}>
+                이게 어떤 원리인거지 도대체
+                <Maps />
+            </div>
+        </>
+    );
 };
 
 export default App;
