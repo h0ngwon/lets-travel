749d1a744cfc763ba294139834768ae21d95de37
Laejun Kim <49670820+Laejun-Kim@users.noreply.github.com>
2023-12-07 19:10:30 +0900

    Test : coords 추가 및 작동 테스트
    부모 컴포넌트에서 data를 받아와 props로 넘겨주면 잘 작동하는 것으로 확인

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,9 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+
+//이 아래로 테스트목적 import
+import MapComponent from 'components/MapComponent';
+import axios from 'axios';
 
 const HomePage = () => {
     const navigate = useNavigate();
+    //~~~~~~~~~~~~테스트 목적~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    const [destCoords, setDestCorrds] = useState();
+    const fetchData = async () => {
+        const { data } = await axios.get(
+            'http://localhost:4000/countryTypeData?country=호주',
+        );
 
+        setDestCorrds(data[0].coords);
+    };
+    useEffect(() => {
+        fetchData();
+    }, []);
+    //~~~~~~~~~~~~~~~테스트 목적~~~~~~~~~~~~~~~~~~~~~~~~~~
     return (
         <div>
             <h1>Homepage</h1>
@@ -14,6 +30,9 @@ const HomePage = () => {
             >
                 임시 여행하러가기
             </button>
+            <div style={{ width: '1000px', height: '600px' }}>
+                <MapComponent destination={destCoords} />
+            </div>
         </div>
     );
 };
