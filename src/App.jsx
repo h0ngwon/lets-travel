77250abdf613f57e3380389d8ce8759d5258ec52
h0ngwon <w___on@naver.com>
2023-12-08 11:01:45 +0900

    Fix: conflicts

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,16 @@
-<<<<<<< HEAD
 import Youtube from 'components/Youtube';
-=======
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
->>>>>>> 40ca68bdcb34bbbc0755f896df3b687695d7b492
 import Router from './shared/Router';
 
 const App = () => {
     const queryClient = new QueryClient();
     return (
-<<<<<<< HEAD
         <div>
             <Youtube />
-=======
-        <QueryClientProvider client={queryClient}>
->>>>>>> 40ca68bdcb34bbbc0755f896df3b687695d7b492
-            <Router />
-        </QueryClientProvider>
+            <QueryClientProvider client={queryClient}>
+                <Router />
+            </QueryClientProvider>
+        </div>
     );
 };
 
