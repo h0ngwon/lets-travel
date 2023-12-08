5aef30e74fc319c40f0e5e68d8ba1180219390af
SuinChoi <ctd2015@gmail.com>
2023-12-08 17:22:37 +0900

    Feat: 로그인/회원가입 기능 구현

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,15 +1,150 @@
 import React from 'react';
+import { useEffect } from 'react';
 import styled from 'styled-components';
 import Swal from 'sweetalert2';
-import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import Navbar from './ui/Navbar';
+import {
+    createUserWithEmailAndPassword,
+    signInWithEmailAndPassword,
+} from 'firebase/auth';
+import { auth } from '../config/firebaseConfig';
 
 const Home = () => {
     const navigate = useNavigate();
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const [passCheck, setPassCheck] = useState('');
+
+    // 회원가입 : 파이어베이스에 email, password 저장
+    const signUp = async (formValues) => {
+        await createUserWithEmailAndPassword(auth, formValues[0], formValues[1])
+            // 회원가입 성공 alert
+            .then(() => {
+                Swal.fire({
+                    position: 'center',
+                    width: 400,
+                    padding: '60px',
+                    icon: 'success',
+                    title: '회원가입 성공!',
+                    showConfirmButton: false,
+                    timer: 1500,
+                }).then(() => {
+                    // 로그인 modal 실행
+                    loginHandler();
+                });
+            })
+            // 에러 코드에 따라 에러 alert
+            .catch((error) => {
+                switch (error.code) {
+                    case 'auth/email-already-in-use':
+                        return Swal.fire({
+                            icon: 'error',
+                            title: '이미 가입된 이메일입니다.',
+                            text: '이메일 주소를 확인해주세요.',
+                            confirmButtonColor: '#00a08d',
+                        }).then(() => {
+                            signupHandler();
+                        });
+                    case 'auth/weak-password':
+                        return Swal.fire({
+                            icon: 'error',
+                            title: '잘못된 비밀번호를 입력하였습니다.',
+                            text: '비밀번호는 6글자 이상이어야 합니다.',
+                            confirmButtonColor: '#00a08d',
+                        }).then(() => {
+                            signupHandler();
+                        });
+                    case 'auth/network-request-failed':
+                        return Swal.fire({
+                            icon: 'error',
+                            title: '네트워크 연결에 실패 하였습니다.',
+                            text: '잠시 후에 다시 시도해 주세요.',
+                            confirmButtonColor: '#00a08d',
+                        }).then(() => {
+                            signupHandler();
+                        });
+                    case 'auth/invalid-email':
+                        return Swal.fire({
+                            icon: 'error',
+                            title: '잘못된 이메일 형식입니다.',
+                            text: '유효한 이메일 형식으로 작성해주세요.',
+                            confirmButtonColor: '#00a08d',
+                        }).then(() => {
+                            signupHandler();
+                        });
+                    default:
+                        return Swal.fire({
+                            icon: 'error',
+                            title: '회원가입에 실패하였습니다.',
+                            text: '이메일 주소와 비밀번호를 확인해주세요.',
+                            confirmButtonColor: '#00a08d',
+                        }).then(() => {
+                            signupHandler();
+                        });
+                }
+            });
+    };
+
+    // 로그인 : 파이어베이스에서 email, password 검색
+    const login = async (formValues) => {
+        try {
+            const userCredential = await signInWithEmailAndPassword(
+                auth,
+                formValues[0],
+                formValues[1],
+            ).then(() => {
+                Swal.fire({
+                    position: 'center',
+                    width: 400,
+                    padding: '60px',
+                    icon: 'success',
+                    title: '로그인 성공!',
+                    showConfirmButton: false,
+                    timer: 1500,
+                });
+            });
+            console.log('현재 로그인한 user : ', auth.currentUser);
+        } catch (error) {
+            console.log('error.code : ', error.code);
+            switch (error.code) {
+                case 'auth/user-not-found" || "auth/wrong-password':
+                case 'auth/network-request-failed':
+                    return Swal.fire({
+                        icon: 'error',
+                        title: '네트워크 연결에 실패 하였습니다.',
+                        text: '잠시 후에 다시 시도해 주세요.',
+                        confirmButtonColor: '#00a08d',
+                    }).then(() => {
+                        loginHandler();
+                    });
+                case 'auth/invalid-email':
+                    return Swal.fire({
+                        icon: 'error',
+                        title: '잘못된 이메일 형식입니다.',
+                        text: '유효한 이메일 형식으로 작성해주세요.',
+                        confirmButtonColor: '#00a08d',
+                    }).then(() => {
+                        loginHandler();
+                    });
+                case 'auth/invalid-credential':
+                    return Swal.fire({
+                        icon: 'error',
+                        title: '잘못된 정보를 입력하였습니다.',
+                        text: '가입한 계정의 정보를 입력해주세요.',
+                        confirmButtonColor: '#00a08d',
+                    }).then(() => {
+                        loginHandler();
+                    });
+                default:
+                    return Swal.fire({
+                        icon: 'error',
+                        title: '로그인에 실패하였습니다.',
+                        text: '이메일 주소와 비밀번호를 확인해주세요.',
+                        confirmButtonColor: '#00a08d',
+                    }).then(() => {
+                        loginHandler();
+                    });
+            }
+        }
+    };
 
     // '여행하러가기' 버튼 클릭 시, 실행
     const askLoginButtonHandler = () => {
@@ -66,8 +201,10 @@ const Home = () => {
             const { value: formValues } = await Swal.fire({
                 title: '로그인',
                 html:
-                    '<label>이메일</label> <input required id="email" type="email" class="swal2-input" placeholder="이메일을 입력해주세요."> <br/>' +
-                    '<label>비밀번호</label> <input id="password" type="password" class="swal2-input" placeholder="비밀번호를 입력해주세요.">',
+                    '<label>이메일</label>' +
+                    '<input required id="email" type="email" class="swal2-input" placeholder="이메일을 입력해주세요."> <br/>' +
+                    '<label>비밀번호</label>' +
+                    '<input id="password" type="password" class="swal2-input" placeholder="비밀번호를 입력해주세요.">',
                 focusConfirm: false,
                 confirmButtonColor: '#00a08d',
                 confirmButtonText: '로그인하기',
@@ -91,9 +228,12 @@ const Home = () => {
             const { value: formValues } = await Swal.fire({
                 title: '회원가입',
                 html:
-                    '<label style="padding:9px">이메일</label> <input id="email" class="swal2-input" placeholder="이메일을 입력하세요."> <br/>' +
-                    '<label>비밀번호</label> <input id="password" class="swal2-input" placeholder="비밀번호를 입력하세요."> <br/>' +
-                    '<label style="padding:18px">확인</label> <input id="passCheck" class="swal2-input" placeholder="비밀번호를 재입력하세요.">',
+                    '<label style="padding:9px">이메일</label>' +
+                    '<input id="email" type="email" class="swal2-input" placeholder="이메일을 입력하세요."> <br/>' +
+                    '<label>비밀번호</label>' +
+                    '<input id="password" type="password" class="swal2-input" placeholder="비밀번호를 입력하세요."> <br/>' +
+                    '<label style="padding:18px">확인</label>' +
+                    '<input id="passCheck" type="password" class="swal2-input" placeholder="비밀번호를 재입력하세요.">',
                 focusConfirm: false,
                 confirmButtonColor: '#00a08d',
                 confirmButtonText: '가입하기',
@@ -118,9 +258,6 @@ const Home = () => {
             // '로그인하기' 버튼 클릭 시, 유효성 검사 실행
             if (formValues.length === 2) {
                 if (formValues[0] !== '' && formValues[1] !== '') {
-                    setEmail(formValues[0]);
-                    setPassword(formValues[1]);
-
                     // 로그인 성공 alert
                     successAlertHandler(formValues);
                 } else {
@@ -148,10 +285,6 @@ const Home = () => {
                             signupHandler();
                         });
                     } else {
-                        setEmail(formValues[0]);
-                        setPassword(formValues[1]);
-                        setPassCheck(formValues[2]);
-
                         // 회원가입 성공 alert
                         successAlertHandler(formValues);
                     }
@@ -164,32 +297,26 @@ const Home = () => {
     };
 
     // 로그인, 회원가입 성공 시, alert 실행
-    const successAlertHandler = (formValues) => {
+    const successAlertHandler = async (formValues) => {
         // 로그인 성공 시, '로그인 성공!' alert
         if (formValues.length === 2) {
-            Swal.fire({
-                position: 'center',
-                width: 400,
-                padding: '60px',
-                icon: 'success',
-                title: '로그인 성공!',
-                showConfirmButton: false,
-                timer: 1500,
-            });
+            await login(formValues);
+            // .then(() => {
+            //     Swal.fire({
+            //         position: 'center',
+            //         width: 400,
+            //         padding: '60px',
+            //         icon: 'success',
+            //         title: '로그인 성공!',
+            //         showConfirmButton: false,
+            //         timer: 1500,
+            //     });
+            // });
         }
         // 회원가입 성공 시, '회원가입 성공!' alert -> 로그인 Modal로 이동
         else {
-            Swal.fire({
-                position: 'center',
-                width: 400,
-                padding: '60px',
-                icon: 'success',
-                title: '회원가입 성공!',
-                showConfirmButton: false,
-                timer: 1500,
-            }).then(() => {
-                loginHandler();
-            });
+            // firebase에 이메일, 비밀번호 저장
+            await signUp(formValues);
         }
     };
 
@@ -197,7 +324,6 @@ const Home = () => {
     const failAlertHandler = (formValues) => {
         // 로그인 실패 시, alert
         if (formValues.length === 2) {
-            console.log('formValues.length : ', formValues.length);
             // 이메일, 비밀번호 둘 다 입력하지 않은 경우
             if (formValues[0] === '' && formValues[1] === '') {
                 Swal.fire({
@@ -267,7 +393,8 @@ const Home = () => {
                     <StH2>여행하자</StH2>
                     <StBtn
                         onClick={() => {
-                            if (email && password) {
+                            if (auth.currentUser) {
+                                console.log(auth.currentUser);
                                 navigate('/survey');
                             } else {
                                 // '여행하러가기' 버튼 클릭 시, 로그인 여부 확인 Modal 실행
@@ -277,8 +404,6 @@ const Home = () => {
                     >
                         여행하러가기
                     </StBtn>
-                    {console.log('email : ', email)}
-                    {console.log('password : ', password)}
                 </StContainer>
             </StPage>
         </>
